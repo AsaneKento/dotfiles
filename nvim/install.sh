aa2f7d2ea6a9056db31707188100f9f58cdcf966
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2023-02-14 08:02:13 +0900

    fix: if .config/nvim directory is nothing

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,6 +6,10 @@ set -ue
 echo ""
 echo "Execute brew doctor..."
 echo ""
+if [ ! -d ${HOME}/.config/nvim ]; then
+  mkdir -p ${HOME}/.config/nvim;
+fi
+
 ln -s -f -v ${PWD}/nvim/init.vim ${HOME}/.config/nvim/init.vim
 
 echo ""
