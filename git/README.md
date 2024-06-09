bbf9b7fdcdb9bf7e53d1e4fbf8d3766603673ac1
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2024-06-09 22:15:02 +0900

    :recycle: gitconfig全体をdotfilesで管理するようにした

--- git/README.md
+++
@@ -1,6 +0,0 @@
-使用する場合は `~/.gitconfig` ファイルに別途以下の記載をお願いします
-
-```
-[include]
-	path = ~/.gitconfig_options
-```
--- git/init.sh
+++ git/init.sh
@@ -8,7 +8,7 @@ echo "Starting Setup git..."
 echo ""
 
 ln -s -f -v ${PWD}/git/.commit_template ${HOME}/.commit_template
-ln -s -f -v ${PWD}/git/.gitconfig_options ${HOME}/.gitconfig_options
+ln -s -f -v ${PWD}/git/.gitconfig ${HOME}/.gitconfig
 
 echo ""
 echo "## Completed Setup git ##"
